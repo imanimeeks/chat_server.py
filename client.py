7dacae7b6ade41bafe02f7f79fe1e12c770ef4df
imanimeeks <52981268+imanimeeks@users.noreply.github.com>
2019-08-06 12:54:53 -0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,68 @@
+# import socket
+# import select
+# import sys
+#
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# if len(sys.argv) != 3:
+#     print "Correct usage: script, IP address, port number"
+#     exit()
+# IP_address = str(sys.argv[1])
+# Port = int(sys.argv[2])
+# server.connect((IP_address, Port))
+#
+# while True:
+#     sockets_list = [sys.stdin, server]
+#     read_sockets,write_socket, error_socket = select.select(sockets_list, [], [])
+#     for socks in read_sockets:
+#         if socks == server:
+#             message = socks.recv(2048)
+#             print message
+#         else:
+#             message = sys.stdin.readline()
+#             server.send(message)
+#             sys.stdout.write("<You>")
+#             sys.stdout.write(message)
+#             sys.stdout.flush()
+# server.close()
+
+import socket
+import select
+import sys
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# if len(sys.argv) != 3:
+#     print ("Correct usage: script, IP address, port number")
+if len(sys.argv) != 4:
+    print("Correct usage: script, IP address, port number")
+    exit()
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+name = sys.argv[3]
+server.connect((IP_address, Port))
+
+
+
+sockets_list = [socket.socket(), server]
+read_sockets,write_socket, error_socket = select.select(sockets_list, [], [])
+while True:
+    for socks in read_sockets:
+        #print("reading sockets")
+        if socks == server:
+            text = socks.recv(2048).decode('utf-8')
+            print(text)
+            #message = sys.stdin.readline()
+            #server.send(message.encode('utf-8'))
+            #sys.stdout.write("<You>")
+            #prompt = 'Write your message: '
+            #message = sys.stdin.readline(prompt)
+            while True:
+                message = input("Write your message: ")
+                spacing = ': '
+                server.send(sys.argv[3].encode('utf-8')+spacing.encode('utf-8')+ message.encode('utf-8'))
+                sys.stdout.write("<You> ")
+                sys.stdout.write(message)
+                print()
+        # sys.stdout.flush()
+sys.stdout.flush()
+server.close()
+server.close()
